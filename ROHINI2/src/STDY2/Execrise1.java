866d72989c225d43ddf8e67ca56639a74dbf55a0
SURESH <SURESH@DESKTOP-HN7VGJP>
2025-08-11 19:15:02 +0530

    FIRST COMMIT

---
+++ ROHINI2/src/STDY2/Execrise1.java
@@ -0,0 +1,17 @@
+package STDY2;
+import java.io.File;
+import java.util.Date;
+
+
+public class Execrise1 {
+	public static void main(String a[])
+	{
+		File file = new File("/home/students/");
+		String[] fileList = file.list();
+		for(String name:fileList) {
+			System.out.println(name);
+		}
+		
+	}
+
+}
